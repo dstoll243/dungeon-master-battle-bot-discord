8d1211f7dd11fdba323839c9f9047bf0a4e6442d
David Stoll <dstoll243@gmail.com>
2020-07-19 15:24:56 -0700

    fixed displayName vs name in models

--- commands/attack.js
+++ commands/attack.js
@@ -5,14 +5,13 @@ module.exports = {
   description: 'attack an enemy',
   async execute(message, args) {
     const [target, attackDamage] = args;
-    console.log(args[0]);
-    const enemy = await Enemy.findOne({ name: target }).exec();
+    const enemy = await Enemy.findOne({ name: `${target}-${message.channel.id}` }).exec();
     if (!enemy) {
       message.channel.send('There is no target by that name');
     } else {
       const totalDamage = enemy.damage + parseInt(attackDamage);
       await enemy.update({ damage: totalDamage });
-      message.channel.send(`${message.author} attacked the ${enemy.name} and they have taken ${totalDamage} total damage`);
+      message.channel.send(`${message.author} attacked the ${enemy.displayName} and they have taken ${totalDamage} total damage`);
     }
   },
 };
--- commands/kill.js
+++ commands/kill.js
@@ -4,12 +4,12 @@ module.exports = {
   name: 'kill',
   description: 'eliminate your enemy',
   async execute(message, args) {
-    const enemy = await Enemy.findOne({ displayName: args[0] }).exec();
+    const enemy = await Enemy.findOne({ name: `${args[0]}-${message.channel.id}` }).exec();
     if (!enemy) {
       message.channel.send('That enemy does not exist to murder');
     } else {
       await Enemy.deleteOne({ name: enemy.name });
-      message.channel.send(`Oh my God! ${message.author} killed ${enemy.name}`);
+      message.channel.send(`Oh my God! ${message.author} killed ${enemy.displayName}`);
     }
   },
 };
