593f28f268b24202ae1233819271b0f2d80de587
Jimmy Chang <jimmy.f.chang94@gmail.com>
2020-07-19 20:47:13 -0700

    adds NaN check for user damage input

--- commands/attack.js
+++ commands/attack.js
@@ -6,10 +6,11 @@ module.exports = {
   async execute(message, args) {
     const [target, attackDamage] = args;
     const enemy = await Enemy.findOne({ name: `${target}-${message.channel.id}` }).exec();
+    const damage = parseInt(attackDamage);
     if (!enemy) {
       message.channel.send('There is no target by that name');
-    } else {
-      const totalDamage = enemy.damage + parseInt(attackDamage);
+    } else if (!isNaN(damage)) {
+      const totalDamage = enemy.damage + damage;
       await enemy.update({ damage: totalDamage });
       message.channel.send(`${message.author} attacked the ${enemy.displayName} and they have taken ${totalDamage} total damage`);
     }
